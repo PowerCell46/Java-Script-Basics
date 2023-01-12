dbe80db4158e7008941a278331fabde8a8d30af0
Peter Gerdzhikov <makotsevo.fan@gmail.com>
2023-01-12 23:35:40 +0200

    Removing tasks

--- Calculator.js
+++
@@ -1,8 +0,0 @@
-function calculator(input) {
-    let depositSum = Number(input[0]);
-    let timeForTheDeposit = Number(input[1]);
-    let yearPersentage = Number(input[2]);
-    let finalSum = depositSum + timeForTheDeposit * ((depositSum * yearPersentage / 100)/ 12);
-    console.log(finalSum);
-}
-calculator(["200", "3", "5.7"]);
--- Converter.js
+++
@@ -1,6 +0,0 @@
-function Converter(input) {
-    let NumberInCM = Number(input[0]);
-    let NumberInInches = NumberInCM * 2.54
-    console.log(NumberInInches)
-}
-Converter([5])
